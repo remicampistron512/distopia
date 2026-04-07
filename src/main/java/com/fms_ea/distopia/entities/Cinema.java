97c6b2ad740e3ba448aca51e334cc2784126a336
Rémi Campistron <remi.campistron@gmail.com>
2026-04-07 08:58:50 +0200

    Ajout de l'entité Cinema

---
+++ src/main/java/com/fms_ea/distopia/entities/Cinema.java
@@ -0,0 +1,29 @@
+package com.fms_ea.distopia.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(exclude = {"city", "showings"})
+public class Cinema {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  private String name;
+  private String address;
+  private double communityRating;
+
+  @ManyToOne
+  @JoinColumn(name = "city_id")
+  private City city;
+
+  @OneToMany(mappedBy = "cinema", cascade = CascadeType.ALL)
+  private List<Showing> showings;
+}
