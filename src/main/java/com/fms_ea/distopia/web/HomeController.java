2160b26d28de0a30bd6b173643f46e949a3e23ae
Rémi Campistron <remi.campistron@gmail.com>
2026-04-07 11:10:02 +0200

    Création du controller pour l'accueil

---
+++ src/main/java/com/fms_ea/distopia/web/HomeController.java
@@ -0,0 +1,23 @@
+package com.fms_ea.distopia.controllers;
+
+import com.fms_ea.distopia.services.HomeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+
+  private final HomeService homeService;
+
+  @GetMapping("/")
+  public String home(Model model) {
+    model.addAttribute("movies", homeService.getFeaturedMovies());
+    model.addAttribute("cinemas", homeService.getTopCinemas());
+    model.addAttribute("cities", homeService.getAllCities());
+    model.addAttribute("showings", homeService.getUpcomingShowings());
+    return "home";
+  }
+}
