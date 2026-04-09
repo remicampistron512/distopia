9472e4e3f8eff4cacd3e19b44fe57b6d029a541a
Rémi Campistron <remi.campistron@gmail.com>
2026-04-09 10:41:33 +0200

    Ajout de divers fichiers manquants pour lancer l'application

---
+++ src/main/java/com/fms_ea/distopia/DistopiaApplication.java
@@ -0,0 +1,13 @@
+package com.fms_ea.distopia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistopiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistopiaApplication.class, args);
+	}
+
+}
